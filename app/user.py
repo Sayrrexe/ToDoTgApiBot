b7eae9cc7996a41399849f3097b02d808828f84f
Sayrrexe <145248257+Sayrrexe@users.noreply.github.com>
2024-08-12 20:47:07 +0500

    main
    start this repository

---
+++ app/user.py
@@ -0,0 +1,28 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart, Command
+import app.keyboards as kb
+from app.database.requests import set_user, del_tasks, set_tasks
+user = Router()
+
+# user.message.middleware(BaseMiddleware())
+
+@user.message(CommandStart())
+async def cmd_start(message: Message):
+    await set_user(message.from_user.id)
+    await message.answer('Нажмите на выполненную задачу, что бы удалить или напишите в чат новую', reply_markup=await kb.tasks(message.from_user.id))
+
+
+@user.callback_query(F.data.startswith('task_'))
+async def delete_task(callback: CallbackQuery):
+    await del_tasks(callback.data.split('_')[1])
+    await callback.message.delete()
+    await callback.message.answer('Нажмите на выполненную задачу, что бы удалить или напишите в чат новую', reply_markup=await kb.tasks(callback.from_user.id))
+    
+@user.message()
+async def set_user_task(message: Message):
+    if len(message.text) > 100:
+        await message.answer('Ваше сообщение превышает лимит в 100 символов')
+        return
+    await set_tasks(message.from_user.id, message.text)
+    await message.answer('Задача добавлена \nНажмите на выполненную задачу, что бы удалить или напишите в чат новую', reply_markup=await kb.tasks(message.from_user.id))
---
+++ config.py
@@ -0,0 +1,5 @@
+TOKEN='7494443400:AAHakVq0FhiuIPR1x4DB2Zq-4KiM5zumB2Y'
+DB_URL='sqlite+aiosqlite:///db.sqlite3'
+#DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
+
+# Внимание! Добавьте config.py в файл .gitignore!
