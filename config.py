cf1f16f76a4468a0d9f3c310c89698612760c8c3
Wise <sayrrexe@gmail.com>
2025-03-02 15:43:21 +0500

    Update Dockerfile

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-TOKEN='7494443400:AAHakVq0FhiuIPR1x4DB2Zq-4KiM5zumB2Y'
+TOKEN=''
 DB_URL='sqlite+aiosqlite:///db.sqlite3'
 #DB_URL='postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
 
-# Внимание! Добавьте config.py в файл .gitignore!
